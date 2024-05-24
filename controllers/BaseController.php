c0f2b15a6345cd9487c06fdf4ad7c67cf94f2abb
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-24 12:20:49 +0300

    Refactor BaseController to improve view rendering and add authentication and authorization handling

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -1,47 +1,40 @@
 <?php
-
 class BaseController
 {
     protected $viewPath = 'views/';
-    protected $layoutPath = 'views/layout/main,php';
+    protected $layout = 'layout/main.php';
 
-    public function render($view, $data = [], $useLayout = true) {
+    // Render a view with optional data
+    protected function render($view, $data = [], $useLayout = true)
+    {
         extract($data);
-
         ob_start(); // Start output buffering
         require $this->viewPath . $view . '.php';
-        $content = ob_get_clean(); // Get the content of the buffer and clean it
+        $content = ob_get_clean(); // Get buffered content and clean buffer
 
         if ($useLayout) {
-            require $this->layoutPath;
+            require $this->viewPath . $this->layout;
         } else {
-            echo $content; // If we don't want to use a layout, just echo the content
+            echo $content; // Directly output the content without layout
         }
     }
 
-    protected function redirect($url) {
-        header('Location: ' . $url);
+    // Redirect to a different URL
+    protected function redirect($url)
+    {
+        header("Location: $url");
         exit;
     }
 
-    protected function back() {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-        exit;
-    }
-
-    protected function checkAuthentication() {
+    protected function checkAuthentication()
+    {
         if (!isset($_SESSION['user_id'])) {
             $this->redirect('/grade-change/login');
         }
     }
 
-    protected function checkAuthorization($role) {
-        if ($_SESSION['role'] != $role) {
-            $this->redirect($this->back());
-        }
-    }
-
-    protected function uploadFile($file, $path) {
+    protected function uploadFile($file, $path)
+    {
         $fileName = uniqid() . '_' . $file['name'];
         move_uploaded_file($file['tmp_name'], $path . $fileName);
         return $fileName;
