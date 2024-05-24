d695a9ffbdb77911ac9e42e8faaf5abc09ba20cd
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-24 12:20:08 +0300

    Refactor DB_NAME constant to use camelCase convention

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'grade_change');
+define('DB_NAME', 'gradeChange');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-function base_url($path = '') {
+function base_url($path = '')
+{
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
     $domainName = $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
     return rtrim($protocol . $domainName, '/') . '/' . ltrim($path, '/');
