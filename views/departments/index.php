d50d1434ce5412fffc2256355c8905e34f207d45
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-28 12:24:00 +0300

    feat: Add departments index view and functionality

---
+++ views/departments/index.php
@@ -0,0 +1,33 @@
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="d-flex align-items-center justify-content-between">
+            <h3>Departments</h3>
+            <a href="<?php echo base_url('departments/create'); ?>" class="btn btn-outline-primary">New Department</a>
+        </div>
+        <table class="table mt-2" style="overflow-x:auto">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Department Head</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $id = 1; ?>
+                <?php foreach ($departments as $department) : ?>
+                    <tr>
+                        <td><?php echo $id++; ?></td>
+                        <td><?php echo $department['name']; ?></td>
+                        <td><?php echo $department['department_head']; ?></td>
+                        <td>
+                            <a href="<?php echo base_url('/departments/edit/' . $department['id']); ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                            <a href="<?php echo base_url('/departments/delete/' . $department['id']); ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                        </td>
+                    </tr>
+                     <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
