b5656ed937f9a58286e0e580a97e9a3c0dc1d41f
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-28 12:24:32 +0300

    feat: Add edit department view and functionality

---
+++ views/departments/edit.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <h3>Edit Department</h3>
+    <form class="row" action="<?php echo base_url('departments/update/' . $department['id']); ?>" method="post">
+
+        <div class="form-group col-6 col-md-4">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" name="name" value="<?php echo $department['name']; ?>" required>
+        </div>
+        
+        <div class="form-group col-6 col-md-4">
+            <label for="role">Department Head:</label>
+            
+            <select class="form-control" name="department_head" id="department_head">
+
+            </select>
+        </div>
+        <div class="form-group col-6 col-md-4 d-flex align-items-end">
+            <button type="submit" class="btn btn-outline-primary ">UPDATE</button>
+        </div>
+    </form>
+</div>
