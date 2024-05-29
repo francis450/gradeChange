872f5941b80b96b9275ec867d062a4244e30265d
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-29 15:51:52 +0300

    refactor: Add CRUD operations for managing faculty members

--- index.php
+++ index.php
@@ -34,4 +34,71 @@ $router->get('/departments/edit/{id}', 'DepartmentController@edit');
 $router->post('/departments/update/{id}', 'DepartmentController@update');
 $router->get('/departments/delete/{id}', 'DepartmentController@delete', checkRole('admin'));
 
+// Faculty
+$router->get('/faculty', 'FacultyController@index');
+$router->get('/faculty/create', 'FacultyController@create');
+$router->post('/faculty/store', 'FacultyController@store');
+$router->get('/faculty/edit/{id}', 'FacultyController@edit');
+$router->post('/faculty/update/{id}', 'FacultyController@update');
+$router->get('/faculty/delete/{id}', 'FacultyController@delete', checkRole('admin'));
+
+// students 
+$router->get('/students', 'StudentController@index');
+$router->get('/students/create', 'StudentController@create');
+$router->post('/students/store', 'StudentController@store');
+$router->get('/students/edit/{id}', 'StudentController@edit');
+$router->post('/students/update/{id}', 'StudentController@update');
+$router->get('/students/delete/{id}', 'StudentController@delete', checkRole('admin'));
+
+// courses
+$router->get('/courses', 'CourseController@index');
+$router->get('/courses/create', 'CourseController@create');
+$router->post('/courses/store', 'CourseController@store');
+$router->get('/courses/edit/{id}', 'CourseController@edit');
+$router->post('/courses/update/{id}', 'CourseController@update');
+$router->get('/courses/delete/{id}', 'CourseController@delete', checkRole('admin'));
+$router->post('/courses/department', 'CourseController@department');
+$router->post('/courses/students', 'CourseController@students');
+
+// enrollments
+$router->get('/enrollments', 'EnrollmentController@index');
+$router->get('/enrollments/create', 'EnrollmentController@create');
+$router->post('/enrollments/store', 'EnrollmentController@store');
+$router->get('/enrollments/edit/{id}', 'EnrollmentController@edit');
+$router->post('/enrollments/update/{id}', 'EnrollmentController@update');
+$router->get('/enrollments/delete/{id}', 'EnrollmentController@delete', checkRole('admin'));
+
+// grades
+$router->get('/grades', 'GradeController@index');
+$router->get('/grades/create', 'GradeController@create');
+$router->post('/grades/store', 'GradeController@store');
+$router->get('/grades/edit/{id}', 'GradeController@edit');
+$router->post('/grades/update/{id}', 'GradeController@update');
+$router->get('/grades/delete/{id}', 'GradeController@delete', checkRole('admin'));
+
+// reports
+$router->get('/reports', 'ReportController@index');
+$router->get('/reports/create', 'ReportController@create');
+$router->post('/reports/store', 'ReportController@store');
+$router->get('/reports/edit/{id}', 'ReportController@edit');
+$router->post('/reports/update/{id}', 'ReportController@update');
+$router->get('/reports/delete/{id}', 'ReportController@delete', checkRole('admin'));
+
+// grade change requests
+$router->get('/grade-change-requests', 'GradeChangeRequestController@index');
+$router->get('/grade-change-requests/create', 'GradeChangeRequestController@create');
+$router->post('/grade-change-requests/store', 'GradeChangeRequestController@store');
+$router->get('/grade-change-requests/edit/{id}', 'GradeChangeRequestController@edit');
+$router->post('/grade-change-requests/update/{id}', 'GradeChangeRequestController@update');
+$router->get('/grade-change-requests/delete/{id}', 'GradeChangeRequestController@delete', checkRole('admin'));
+
+
+// notifications
+$router->get('/notifications', 'NotificationController@index');
+$router->get('/notifications/create', 'NotificationController@create');
+$router->post('/notifications/store', 'NotificationController@store');
+$router->get('/notifications/edit/{id}', 'NotificationController@edit');
+$router->post('/notifications/update/{id}', 'NotificationController@update');
+$router->get('/notifications/delete/{id}', 'NotificationController@delete', checkRole('admin'));
+
 $router->resolve();
