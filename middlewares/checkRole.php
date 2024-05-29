67fb690ded483dc6bd16bef4b8662ee273a2060a
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-29 15:50:50 +0300

    refactor: Remove session_start() from checkRole middleware

--- middlewares/checkRole.php
+++ middlewares/checkRole.php
@@ -1,7 +1,6 @@
 <?php
 function checkRole($requiredRole) {
     return function() use ($requiredRole) {
-        session_start();
         if (!isset($_SESSION['role']) || $_SESSION['role'] !== $requiredRole) {
             http_response_code(403);
             echo "Access denied";
