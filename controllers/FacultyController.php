1d6ccee744037f40b888f02009944741d8361d41
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-29 15:51:31 +0300

    refactor: Improve code structure and readability of FacultyController

---
+++ controllers/FacultyController.php
@@ -0,0 +1,123 @@
+<?php
+
+class FacultyController extends BaseController
+{
+    public function __construct()
+    {
+        $this->checkAuthentication();
+    }
+
+    public function index()
+    {
+        $faculty = new FacultyMember();
+        $user = new User();
+        $department = new Department();
+
+        $faculty = $faculty->all();
+
+        foreach ($faculty as &$f) {
+            if ($f['department_id']) {
+                $f['department_name'] = $department->find($f['department_id'])['name'];
+            } else {
+                $f['department_name'] = '';
+            }
+            $firstname = $user->find($f['user_id'])['firstname'];
+            $lastname = $user->find($f['user_id'])['lastname'];
+            $f['faculty_member_name'] = $firstname . ' ' . $lastname;
+        }
+        unset($f);
+
+
+        $this->render('faculty/index', compact('faculty'));
+    }
+
+    public function create()
+    {
+        $userModel = new User();
+        $departments = new Department();
+        $faculty = new FacultyMember();
+
+        $departments = $departments->all();
+        $users = $userModel->where('role', 'user');
+        $faculty = $faculty->all();
+
+        // remove users whose user_id is already in faculty
+        foreach ($faculty as $f) {
+            foreach ($users as $key => $user) {
+                if ($f['user_id'] == $user['id']) {
+                    unset($users[$key]);
+                }
+            }
+        }
+
+        $this->render('faculty/create', compact('departments', 'users'));
+    }
+
+    public function store()
+    {
+        $faculty = new FacultyMember();
+        $userModel = new User();
+
+        $data = [
+            'user_id' => $_POST['name'],
+            'department_id' => $_POST['department'],
+            'role' => $_POST['role']
+        ];
+
+        if ($faculty->create($data)) {
+            $userModel->update('id', $_POST['name'], ['role' => 'faculty member']);
+            $this->redirect(base_url('/faculty'));
+        } else {
+            return 'faculty creation failed';
+        }
+    }
+
+    public function edit($params)
+    {
+        $faculty = new FacultyMember;
+        $department = new Department;
+
+        $faculty = $faculty->find($params);
+        $department = $department->all();
+
+        $this->render('faculty/edit', compact('faculty', 'department'));
+    }
+
+    public function update($params)
+    {
+        $data = [
+            'role' => $_POST['role'],
+            'department_id' => $_POST['department']
+        ];
+
+        $faculty = new FacultyMember();
+
+        $faculty->update('id', $params[0], $data);
+
+        if ($faculty) {
+            $this->redirect(base_url('/faculty'));
+        } else {
+            return 'faculty update failed';
+        }
+    }
+
+    public function delete($params)
+    {
+        $facultyModel = new FacultyMember();
+        $userModel = new User();
+
+        $faculty = $facultyModel->find($params);
+
+        if ($faculty) {
+            $user = $userModel->update('id', $faculty['user_id'], ['role' => 'user']);
+            echo '<pre>';
+            print_r($user);
+            echo '<pre>';
+            if ($facultyModel->delete($params)) {
+                $this->redirect(base_url('/faculty'));
+            } else {
+                return 'faculty deletion failed';
+            }
+        }
+    }
+}
