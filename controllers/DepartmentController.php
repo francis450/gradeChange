c55211a8ba4f67d95f235dc76bca372ae612ef87
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-29 14:39:10 +0300

    feat: Add department head information to departments index view

--- controllers/DepartmentController.php
+++ controllers/DepartmentController.php
@@ -2,35 +2,67 @@
 
 class DepartmentController extends BaseController
 {
+
     public function __construct()
     {
         $this->checkAuthentication();
     }
+
     public function index()
     {
         $departmentModel = new Department();
+        $facultyModel = new FacultyMember();
+        $userModel = new User();
 
         $departments = $departmentModel->all();
+
+        foreach ($departments as &$department) {
+            $faculty = $facultyModel->where('department_id', $department['id']);
+            if ($faculty) {
+                foreach ($faculty as &$f) {
+                    if ($f['role'] == 'department head') {
+                        $user = $userModel->find($f['user_id']);
+                        $firstname = $user['firstname'];
+                        $lastname = $user['lastname'];
+                        $department['department_head'] = $firstname . ' ' . $lastname;
+                    } else {
+                        $department['department_head'] = '';
+                    }
+                }
+            }else{
+                $department['department_head'] = '';
+            }
+        }
 
         $this->render('departments/index', compact('departments'));
     }
 
     public function create()
     {
-        $faculty = new FacultyMember();
-        $faculty = $faculty->all();
-        $this->render('departments/create', compact('faculty'));
+        // $faculty = new FacultyMember();
+        // $userModel = new User();
+
+        // $faculty = $faculty->where('role', 'regular faculty');
+
+        // foreach($faculty as &$f) {
+        //     $firstname = ($userModel->find($f['user_id']))['firstname'];
+        //     $lastname = $userModel->find($f['user_id'])['lastname'];
+        //     $f['name'] = $firstname.' '.$lastname;
+        // }
+
+        $this->render('departments/create');
     }
 
     public function store()
     {
-        $data = [
-           "name" => $_POST['name'],
-            "department_head" => $_POST['department_head']
-        ];
         $departmentModel = new Department();
+
+        $data = [
+            "name" => $_POST['name']
+        ];
+
         $departmentModel = $departmentModel->create($data);
-        if($departmentModel){
+        if ($departmentModel) {
             $this->redirect(base_url('/departments'));
         } else {
             return 'Department creation failed';
@@ -49,15 +81,14 @@ class DepartmentController extends BaseController
     public function update($params)
     {
         $data = [
-            'name' => $_POST['name'],
-            'department_head' => $_POST['department_head']
+            'name' => $_POST['name']
         ];
 
         $departmentModel = new Department();
 
-        $departmentModel->update('id',$params[0], $data);
+        $departmentModel->update('id', $params[0], $data);
 
-        if($departmentModel) {
+        if ($departmentModel) {
             $this->redirect(base_url('/departments'));
         } else {
             return 'Department update failed';
@@ -67,7 +98,7 @@ class DepartmentController extends BaseController
     public function delete($params)
     {
         $departmentModel = new Department();
-        if($departmentModel->delete($params[0])){
+        if ($departmentModel->delete($params[0])) {
             $this->redirect(base_url('/departments'));
         } else {
             return 'Department deletion failed';
