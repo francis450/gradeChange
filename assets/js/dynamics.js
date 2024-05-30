2bb3ddd0a370413c5c87696438f15d459c51f98c
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-30 10:55:34 +0300

    refactor: Add input validation for points field in dynamics.js

--- assets/js/dynamics.js
+++ assets/js/dynamics.js
@@ -74,4 +74,82 @@ $(document).ready(function () {
       });
     }
   });
+
+  $("#points").on("input", function () {
+    var points = $(this).val();
+    if (points < 0 || points > 100 || !$.isNumeric(points)) {
+      return;
+    }
+
+    if (points >= 80) {
+      $("#grade").val("A");
+    } else if (points >= 70) {
+      $("#grade").val("B");
+    } else if (points >= 60) {
+      $("#grade").val("C");
+    } else if (points >= 50) {
+      $("#grade").val("D");
+    } else if (points >= 40) {
+      $("#grade").val("E");
+    } else if (points < 40) {
+      $("#grade").val("F");
+    } else {
+      alert("Invalid points");
+    }
+  });
+  
+  $(".ernrollment_student_id").change(function () {
+    var student_id = $(this).val();
+    var pathArray = window.location.pathname.split("/");
+    let url = pathArray[0] + "/" + pathArray[1] + "/enrollments/student";
+    if (student_id != "") {
+      $.ajax({
+        url: url,
+        method: "POST",
+        data: { student_id: student_id },
+        dataType: "json",
+        success: function (data) {
+          // console.log(data);
+          var enrollmentCourseSelect = $(".course_grade_id");
+          enrollmentCourseSelect.empty();
+          enrollmentCourseSelect.append(
+            '<option value="">Select Course</option>'
+          );
+          $.each(data, function (key, value) {
+            enrollmentCourseSelect.append(
+              '<option value="' + value.id + '">' + value.name + "</option>"
+            );
+          });
+        },
+      });
+    }
+  });
+
+  $(".course_grade_id").change(function () {
+    var course_id = $(this).val();
+    var student_id = $("#student_id").val();
+
+    var pathArray = window.location.pathname.split("/");
+    let url = pathArray[0] + "/" + pathArray[1] + "/grades/course";
+    if (course_id != "" && student_id != "") {
+      $.ajax({
+        url: url,
+        method: "POST",
+        data: { course_id: course_id, student_id: student_id },
+        dataType: "json",
+        success: function (data) {
+          var data = data[0];
+          var original_points = $("#original_points");
+          original_points.empty();
+          original_points.val(data.points); // Corrected line
+
+          var original_grade = $("#original_grade");
+          original_grade.empty();
+          original_grade.val(data.grade); // Corrected line
+        },
+      });
+    } else {
+      alert("Choose Course and Student to load grades");
+    }
+  });
 });
